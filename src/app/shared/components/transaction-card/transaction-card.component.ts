558dbfc09a940ad071c221797a796566b6152315
Mert Tamer <mert_tamer@hotmail.com>
2025-02-12 13:27:24 +0100

    feat: setup basic component structure

---
+++ src/app/shared/components/transaction-card/transaction-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transaction-card',
+  imports: [],
+  templateUrl: './transaction-card.component.html',
+  styleUrl: './transaction-card.component.css'
+})
+export class TransactionCardComponent {
+
+}
---
+++ src/app/transactions/transaction-detail/transaction-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transaction-detail',
+  imports: [],
+  templateUrl: './transaction-detail.component.html',
+  styleUrl: './transaction-detail.component.css'
+})
+export class TransactionDetailComponent {
+
+}
