7ee99f8398cd4cd7d5eb88c4f716a6b182bfd0a3
Mert Tamer <mert_tamer@hotmail.com>
2025-02-12 16:30:47 +0100

    feat: setup basic ui and styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./core/components/header/header.component";
+import { NavigationComponent } from "./core/components/navigation/navigation.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, NavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'rabobank-assignment';
+
 }
