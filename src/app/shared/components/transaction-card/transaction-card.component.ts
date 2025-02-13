8ee64cf76dc0c8228939b8eda51629db42a41976
Mert Tamer <mert_tamer@hotmail.com>
2025-02-13 12:34:30 +0100

    feat: developed transaction-list and transaction-card components

--- src/app/shared/components/transaction-card/transaction-card.component.ts
+++ src/app/shared/components/transaction-card/transaction-card.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Transactions } from '../../../core/models/transactions.model';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-transaction-card',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './transaction-card.component.html',
   styleUrl: './transaction-card.component.css'
 })
 export class TransactionCardComponent {
-
+  @Input() transactionsByDay!: Transactions;
 }
--- src/app/transactions/transaction-list/transaction-list.component.ts
+++ src/app/transactions/transaction-list/transaction-list.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { Transactions } from '../../core/models/transactions.model';
 import { TransactionService } from '../../core/services/transaction.service';
+import { TransactionCardComponent } from "../../shared/components/transaction-card/transaction-card.component";
 
 @Component({
   selector: 'app-transaction-list',
-  imports: [],
+  imports: [TransactionCardComponent],
   templateUrl: './transaction-list.component.html',
   styleUrl: './transaction-list.component.css'
 })
@@ -14,7 +15,16 @@ export class TransactionListComponent {
   private transactionService = inject(TransactionService);
 
   ngOnInit() {
-    this.transactionService.getTransactions().subscribe(data => this.transactionsByDay = data);
+    this.transactionService.getTransactions().subscribe(data => {
+      this.transactionsByDay = data
+        .map(day => ({
+          ...day,
+          transactions: day.transactions.sort((a, b) =>
+            new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime()
+          )
+        }))
+        .sort((a, b) => new Date(b.id).getTime() - new Date(a.id).getTime());
+    });
   }
 
 }
