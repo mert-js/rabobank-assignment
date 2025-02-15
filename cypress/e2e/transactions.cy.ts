d2541819680d3995162ee66a53eb1a2b731b2293
Mert Tamer <mert_tamer@hotmail.com>
2025-02-15 19:51:57 +0100

    feat: developed basic e2e test

--- cypress/e2e/transactions.cy.ts
+++ cypress/e2e/transactions.cy.ts
@@ -1,6 +1,25 @@
-describe('My First Test', () => {
-  it('Visits the initial project page', () => {
-    cy.visit('/')
-    cy.contains('app is running')
-  })
+describe('Transactions E2E test', () => {
+  beforeEach(() => {
+    cy.intercept('GET', '/api/transactions', {
+      fixture: 'transactions.json',
+    }).as('getTransactions');
+    cy.visit('/');
+  });
+
+  it('should display the transactions list sorted by days', () => {
+    cy.wait('@getTransactions');
+
+    cy.get('.transaction-card').then((cards) => {
+      const days = [...cards].map(card => card.querySelector('p')?.innerText);
+      expect(days).to.deep.equal(['8 november 2022', '6 november 2022', '5 november 2022', '2 november 2022']);
+    });
+  });
+
+  it('should navigate to the transaction details page when a transaction is clicked', () => {
+    cy.wait('@getTransactions');
+
+    cy.get('.transaction-card a').first().click();
+
+    cy.url().should('include', '/transactions/2022-11-08/1');
+  });
 })
